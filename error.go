678c649159ae1513da4d678f9935f4e5bf5dbba2
hassieswift621 <hassie.dev@gmail.com>
2019-08-07 15:14:57 +0100

    Switch to xerrors

--- error.go
+++ error.go
@@ -1,29 +1,31 @@
 package tatsumakigo
 
-import "errors"
+import (
+	"golang.org/x/xerrors"
+)
 
 var (
 	errorRequestFailed = func(err error) error {
-		return errors.New("tatsumakigo: Failed to create request: " + err.Error())
+		return xerrors.Errorf("tatsumakigo: Failed to create request: %w", err)
 	}
 
 	errorResponseFailed = func(err error) error {
 		if err != nil {
-			return errors.New("tatsumakigo: Failed to get response: " + err.Error())
-		} else {
-			return errors.New("tatsumakigo: Failed to get response")
+			return xerrors.Errorf("tatsumakigo: Failed to get response: %w", err)
 		}
+
+		return xerrors.New("tatsumakigo: Failed to get response")
 	}
 
 	errorParseFailed = func(err error) error {
-		return errors.New("tatsumakigo: Failed to parse response: " + err.Error())
+		return xerrors.Errorf("tatsumakigo: Failed to parse response: %w", err)
 	}
 
 	errorAdjustInvalid = func() error {
-		return errors.New("tatsumakigo: The amount to adjust for remove and set actions must be above 0")
+		return xerrors.New("tatsumakigo: The amount to adjust for remove and set actions must be above 0")
 	}
 
 	errorAdjustBounds = func() error {
-		return errors.New("tatsumakigo: The amount to adjust must be between 0 and 50,000 (inclusive)")
+		return xerrors.New("tatsumakigo: The amount to adjust must be between 0 and 50,000 (inclusive)")
 	}
 )
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/hassieswift621/tatsumaki-go
 
 go 1.12
 
-require golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7 // indirect
+require golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7
