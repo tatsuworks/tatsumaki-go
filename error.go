387485aff11ff49b14125f818bd61da7a7ee580a
hassieswift621 <hassie.dev@gmail.com>
2019-11-30 16:38:26 +0000

    Improvements to error message style

--- error.go
+++ error.go
@@ -6,26 +6,26 @@ import (
 
 var (
 	errorRequestFailed = func(err error) error {
-		return xerrors.Errorf("tatsumakigo: Failed to create request: %w", err)
+		return xerrors.Errorf("tatsumakigo: failed to create request: %w", err)
 	}
 
 	errorResponseFailed = func(err error) error {
 		if err != nil {
-			return xerrors.Errorf("tatsumakigo: Failed to get response: %w", err)
+			return xerrors.Errorf("tatsumakigo: failed to get response: %w", err)
 		}
 
-		return xerrors.New("tatsumakigo: Failed to get response")
+		return xerrors.New("tatsumakigo: failed to get response")
 	}
 
 	errorParseFailed = func(err error) error {
-		return xerrors.Errorf("tatsumakigo: Failed to parse response: %w", err)
+		return xerrors.Errorf("tatsumakigo: failed to parse response: %w", err)
 	}
 
 	errorAdjustInvalid = func() error {
-		return xerrors.New("tatsumakigo: The amount to adjust for remove and set actions must be above 0")
+		return xerrors.New("tatsumakigo: the amount to adjust for remove and set actions must be above 0")
 	}
 
 	errorAdjustBounds = func() error {
-		return xerrors.New("tatsumakigo: The amount to adjust must be between 0 and 50,000 (inclusive)")
+		return xerrors.New("tatsumakigo: the amount to adjust must be between 0 and 50,000 (inclusive)")
 	}
 )
