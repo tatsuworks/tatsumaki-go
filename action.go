695aac99cbd453c9570ba267d7271df9f37e447f
hassieswift621 <hassie.dev@gmail.com>
2019-12-03 10:58:39 +0000

    Documented guild update actions

--- action.go
+++ action.go
@@ -1,10 +1,13 @@
 package tatsumakigo
 
-// Action is the action type when updating a guild.
+// Action is the action type for updating a user's guild points or score.
 type Action string
 
 const (
-	ActionAdd    Action = "add"
+	// ActionAdd is the action for adding a user's guild points or score.
+	ActionAdd Action = "add"
+	// ActionRemove is the action for removing a user's guild points or score.
 	ActionRemove Action = "remove"
-	ActionSet    Action = "set"
+	// ActionSet is the action for setting a user's guild points or score.
+	ActionSet Action = "set"
 )
