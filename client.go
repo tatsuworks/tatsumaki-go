e6c0ec5b7dec4713f7b467dea7eb489759eb1a4e
hassieswift621 <hassie.dev@gmail.com>
2019-12-03 11:12:58 +0000

    Add WithContext call functions

--- client.go
+++ client.go
@@ -14,30 +14,58 @@ func New(token string) *Client {
 	}
 }
 
-// AdjustGuildUserPoints adjusts a user's points in a guild.
-// The amount must be between 0 and 50,000 (inclusive) and must be above 0 if the action is add or remove.
+// AdjustGuildUserPoints wraps AdjustGuildUserPointsWithContext using the background context.
 func (t *Client) AdjustGuildUserPoints(guildID string, userID string, amount int, action Action) (*GuildUserPoints, error) {
-	return t.restClient.adjustGuildUserPoints(context.Background(), guildID, userID, amount, action)
+	return t.AdjustGuildUserPointsWithContext(context.Background(), guildID, userID, amount, action)
 }
 
-// AdjustGuildUserScore adjusts a user's score in a guild.
+// AdjustGuildUserPointsWithContext adjusts a user's points in a guild.
 // The amount must be between 0 and 50,000 (inclusive) and must be above 0 if the action is add or remove.
-func (t *Client) AdjustGuildUserScore(guildID string, userID string, amount int, action Action) (*GuildUserScore, error) {
-	return t.restClient.adjustGuildUserScore(context.Background(), guildID, userID, amount, action)
+func (t *Client) AdjustGuildUserPointsWithContext(ctx context.Context, guildID string, userID string, amount int,
+	action Action) (*GuildUserPoints, error) {
+	return t.restClient.adjustGuildUserPoints(ctx, guildID, userID, amount, action)
 }
 
-// GuildLeaderboard gets the leaderboard for a guild.
+// AdjustGuildUserScore wraps AdjustGuildUserScoreWithContext using the background context.
+func (t *Client) AdjustGuildUserScore(guildID string, userID string, amount int,
+	action Action) (*GuildUserScore, error) {
+	return t.AdjustGuildUserScoreWithContext(context.Background(), guildID, userID, amount, action)
+}
+
+// AdjustGuildUserScoreWithContext adjusts a user's score in a guild.
+// The amount must be between 0 and 50,000 (inclusive) and must be above 0 if the action is add or remove.
+func (t *Client) AdjustGuildUserScoreWithContext(ctx context.Context, guildID string, userID string, amount int,
+	action Action) (*GuildUserScore, error) {
+	return t.restClient.adjustGuildUserScore(ctx, guildID, userID, amount, action)
+}
+
+// GuildLeaderboard wraps GuildLeaderboardWithContext using the background context.
 func (t *Client) GuildLeaderboard(guildID string) ([]*GuildRankedUser, error) {
-	return t.restClient.guildLeaderboard(context.Background(), guildID)
+	return t.GuildLeaderboardWithContext(context.Background(), guildID)
 }
 
-// GuildUserStats gets a user's stats for a guild.
-// @me is accepted for the user ID, which will retrieve stats for yourself.
+// GuildLeaderboardWithContext gets the leaderboard for a guild.
+func (t *Client) GuildLeaderboardWithContext(ctx context.Context, guildID string) ([]*GuildRankedUser, error) {
+	return t.restClient.guildLeaderboard(ctx, guildID)
+}
+
+// GuildUserStats wraps GuildUserStatsWithContext using the background context.
 func (t *Client) GuildUserStats(guildID string, userID string) (*GuildUserStats, error) {
-	return t.restClient.guildUserStats(context.Background(), guildID, userID)
+	return t.GuildUserStatsWithContext(context.Background(), guildID, userID)
+}
+
+// GuildUserStatsWithContext gets a user's stats for a guild.
+// @me is accepted for the user ID, which will retrieve stats for the owner of the API token.
+func (t *Client) GuildUserStatsWithContext(ctx context.Context, guildID string, userID string) (*GuildUserStats, error) {
+	return t.restClient.guildUserStats(ctx, guildID, userID)
+}
+
+// User wraps UserWithContext using the background context.
+func (t *Client) User(userID string) (*User, error) {
+	return t.UserWithContext(context.Background(), userID)
 }
 
 // User gets a Tatsumaki user profile.
-func (t *Client) User(userID string) (*User, error) {
-	return t.restClient.user(context.Background(), userID)
+func (t *Client) UserWithContext(ctx context.Context, userID string) (*User, error) {
+	return t.restClient.user(ctx, userID)
 }
