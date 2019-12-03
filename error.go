53f0a3767a81f6e27bd6a2f9f1cb9111168fde3a
hassieswift621 <hassie.dev@gmail.com>
2019-12-03 10:53:33 +0000

    Made errors functions, fixed error msg for invalid guild point adjustments

--- error.go
+++ error.go
@@ -4,28 +4,26 @@ import (
 	"golang.org/x/xerrors"
 )
 
-var (
-	errorRequestFailed = func(err error) error {
-		return xerrors.Errorf("tatsumakigo: failed to create request: %w", err)
+func errorRequestFailed(err error) error {
+	return xerrors.Errorf("tatsumakigo: failed to create request: %w", err)
+}
+
+func errorResponseFailed(err error) error {
+	if err != nil {
+		return xerrors.Errorf("tatsumakigo: failed to get response: %w", err)
 	}
 
-	errorResponseFailed = func(err error) error {
-		if err != nil {
-			return xerrors.Errorf("tatsumakigo: failed to get response: %w", err)
-		}
+	return xerrors.New("tatsumakigo: failed to get response")
+}
 
-		return xerrors.New("tatsumakigo: failed to get response")
-	}
+func errorParseFailed(err error) error {
+	return xerrors.Errorf("tatsumakigo: failed to parse response: %w", err)
+}
 
-	errorParseFailed = func(err error) error {
-		return xerrors.Errorf("tatsumakigo: failed to parse response: %w", err)
-	}
+func errorAdjustInvalid() error {
+	return xerrors.New("tatsumakigo: the amount to adjust for add and remove actions must be above 0")
+}
 
-	errorAdjustInvalid = func() error {
-		return xerrors.New("tatsumakigo: the amount to adjust for remove and set actions must be above 0")
-	}
-
-	errorAdjustBounds = func() error {
-		return xerrors.New("tatsumakigo: the amount to adjust must be between 0 and 50,000 (inclusive)")
-	}
-)
+func errorAdjustBounds() error {
+	return xerrors.New("tatsumakigo: the amount to adjust must be between 0 and 50,000 (inclusive)")
+}
